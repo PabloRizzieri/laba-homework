64886eb11b36c2977bcb66f3824983f2e7a884a3
Pablo Rizzieri <ppablorizzieri@gmail.com>
2023-11-09 14:09:47 -0300

    Rework on the Supermarket, changed the constructor to make it more useful and meaningfully on the project using two new parameters making the class have an actual staff and products to offer

--- src/main/Supermarket.java
+++ src/main/Supermarket.java
@@ -1,20 +1,30 @@
 package main;
 
+import main.products.Combo;
+import main.staff.Employees;
+
 public class Supermarket {
     private String name;
     private String location;
+
+    private Employees staff;
+    private Combo products;
 
     // Default constructor for the class
     public Supermarket(){
         this.name = "default";
         this.location = "default";
+        this.products = null;
+        this.staff = null;
 
     }
 
     //Constructor for the Supermarket class
-    public Supermarket(String name, String location){
+    public Supermarket(String name, String location, Employees staff, Combo products){
         this.name = name;
         this.location = location;
+        this.staff = staff;
+        this.products = products;
     }
 
     //Method to print the class information
@@ -30,6 +40,26 @@ public class Supermarket {
     // Getter and Setter of the name property
     public void setName(String name){ this.name = name; }
     public String getName (){ return name; }
+
+
+    // Getter and Setter of the staff property
+    public Employees getStaff() {
+        return staff;
+    }
+
+    public void setStaff(Employees staff) {
+        this.staff = staff;
+    }
+
+
+    // Getter and Setter of the products property
+    public Combo getProducts() {
+        return products;
+    }
+
+    public void setProducts(Combo products) {
+        this.products = products;
+    }
 
 
 
