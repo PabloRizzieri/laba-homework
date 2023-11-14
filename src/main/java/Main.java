8c1536e53219ccc22dac0027abb21aa14d249b11
Pablo Rizzieri <ppablorizzieri@gmail.com>
2023-11-14 14:31:03 -0300

    Restructure and testing of the new methods and functionalities created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ package main.java;
 
 import main.java.exceptions.*;
 import main.java.products.*;
+import main.java.providers.*;
 import main.java.staff.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.*;
 
 
 public class Main {
@@ -26,31 +29,31 @@ public class Main {
         CleaningProduct cleaningProduct = new CleaningProduct("Soap", 34, 781, "Cleaning", false, 10);
 
         //Creating instances of the subclasses that Supermarket has
+        Set<Person> staff2 = Employees.createStaff(registerEmployee, supervisorEmployee, depositEmployee, cleaningEmployee);
         Employees staff = new Employees("Monday to Friday", "Vea","Buenos Aires", cleaningEmployee, depositEmployee, registerEmployee, supervisorEmployee);
-        Combo actualCombo = new Combo("Mixed Combo", 22, meatProduct, cerealProduct, appleProduct1, cleaningProduct);
+        Combo actualCombo = new Combo("Variadito", meatProduct, cerealProduct, appleProduct1, cleaningProduct);
+        Combo actualCombo1 = new Combo("Variadito1", meatProduct, cerealProduct, appleProduct1, cleaningProduct);
+        List<Product> listCombo = Combo.createCombo("Variadito2", meatProduct, cerealProduct, appleProduct2, cleaningProduct);
 
 
         // Testing new usage and creation of Supermarket
         Supermarket supermarket = new Supermarket("VEA", "RESISTENCIA", staff, actualCombo);
         LOGGER.info("The PRODUCTS OF THE MARKET ARE: " + supermarket.getStaff());
 
-        LOGGER.info(meatProduct.getPrice() + " " + meatProduct.isPriceable());
+
 
         //Using the default constructors
         Employees defaultStaff = new Employees();
         Combo defaultCombo = new Combo();
 
         // Printing specific data about the classes
-        LOGGER.info(actualCombo.getComboName());
         LOGGER.info(staff.getCleaningEmployee().getFirstName());
         LOGGER.info("The name of the deposit employee is " + staff.getDepositEmployee().getFirstName());
-
-
         staff.printInfo();
         staff.printDetails();
         cleaningEmployee.printDetails();
 
-        defaultCombo.printInfo();
+        // defaultCombo.printInfo(); Testing printInfo method
 
         // Utilizing the override method of the parent class
         appleProduct1.printProductInfo();
@@ -69,13 +72,58 @@ public class Main {
         // Testing toString method
         LOGGER.info(depositEmployee.toString());
         LOGGER.info(appleProduct1.toString());
+        LOGGER.info(actualCombo1.toString());
+
+
+        /* Utilizing the new methods of the final Provider class
+        *   Provider provider = new Provider();
+            provider.deliverMerchandise();
+            Provider.deliverSchedule();
+        * */
+
+
+
+        // Testing Providers method to create a TreeMap and then printing the values of the new Treemap
+        MeatProvider meatProvider = new MeatProvider("Carnes San Carlos");
+        FruitProvider fruitProvider = new FruitProvider("Frutas Martinez");
+        TreeMap<Integer, Provider> providers = Provider.createProvidersCollection(meatProvider, fruitProvider);
+        providers.values().forEach(provider -> LOGGER.info("Provider name: " + provider.getProviderName()));
+
+        // Testing new Combo methods to get the details of the combo and create a LinkedList of combos
+        LOGGER.info(actualCombo.getName(1));
+        LOGGER.info(actualCombo1.getId("Variadito1"));
+        LOGGER.info(actualCombo1.getName(2));
         LOGGER.info(actualCombo.toString());
+        LOGGER.info(listCombo.get(1));
+
+        // Testing access to the comboDetails HashMap to obtain the key and the value
+        LOGGER.info(actualCombo1.getComboDetails().values() + " " + actualCombo1.getComboDetails().keySet());
+
+        // Testing the new method on the Combo class to create a LinkedList with all the combos provided on the parameters
+        LinkedList<Combo> allCombos = Combo.createComboList("Variaditos", actualCombo1, actualCombo);
+        LOGGER.info(allCombos.get(0).getComboDetails());
+
+        //Testing the staff2 created with the createStaff method
+        LOGGER.info(staff2);
+        LOGGER.info(staff2.contains("Rizzieri"));
+        LOGGER.info(staff2.contains("Marianito"));
+
+        // Testing a loop for print specific data about the objects inside the HashSet
+        for (Person employee : staff2) {
+            // Check the person type and realize a casting
+            if (employee instanceof CleaningEmployee) {
+                CleaningEmployee cleaningEmployeeTest = (CleaningEmployee) employee;
+                LOGGER.info("Name of the cleaning employee: " + cleaningEmployeeTest.getFirstName());
+                // Can get any other attributes that I need here
+            } else if (employee instanceof RegisterEmployee) {
+                RegisterEmployee employeeRegister = (RegisterEmployee) employee;
+                LOGGER.info("Name of the register: " + employeeRegister.getFirstName());
+                // Can get any other attributes that I need here
+            }
+        }
 
 
-        // Utilizing the new methods of the final Provider class
-        Provider provider = new Provider();
-        provider.deliverMerchandise();
-        Provider.deliverSchedule();
+
 
 
         //Handling Exceptions with try-catch block
@@ -83,25 +131,27 @@ public class Main {
             appleProduct1.validateProductID();
         } catch (InvalidIDException e){
             LOGGER.info("Caught an exception: " + e.getMessage());
+            e.printStackTrace();
         }
 
         try {
             meatProduct.validateBranch();
         } catch (InvalidBranchException e){
             LOGGER.info("Caught an exception: " + e.getMessage());
+            e.printStackTrace();
         }
 
         try {
             cerealProduct.validatePrice();
-        } catch (NoPriceException e){
+        } catch (NoPriceException | NoStockException e){
             LOGGER.info("Caught an exception: " + e.getMessage());
+            e.printStackTrace();
         }
 
 
         // Using the second way to handle exceptions
         checkValidAge(cleaningEmployee);
         checkValidName(registerEmployee);
-
 
 
 
@@ -114,6 +164,7 @@ public class Main {
             person.checkPersonAge();
         } catch (NoAgeException e){
             LOGGER.info("INVALID AGE");
+            e.printStackTrace();
         }
     }
 
@@ -122,6 +173,7 @@ public class Main {
             person.checkPersonName();
         } catch (InvalidPersonNameException e){
             LOGGER.info("INVALID NAME");
+            e.printStackTrace();
         }
     }
 
