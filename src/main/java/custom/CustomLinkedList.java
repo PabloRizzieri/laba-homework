406981cf4e124a832c85f54c12ad3666909f8ef5
Pablo Rizzieri <ppablorizzieri@gmail.com>
2023-11-27 14:51:03 -0300

    Creation of new methods to use on the custom linked list

--- src/main/java/custom/CustomLinkedList.java
+++ src/main/java/custom/CustomLinkedList.java
@@ -1,5 +1,7 @@
 package custom;
 
+import java.util.Comparator;
+
 public class CustomLinkedList<T> {
     private Node<T> head;
     private int size;
@@ -17,6 +19,63 @@ public class CustomLinkedList<T> {
         head = newNode;
         size++;
     }
+
+    // Method to check if the list is empty
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    // Method to get the size of the list
+    public int size() {
+        return size;
+    }
+
+    public void sort(Comparator<T> comparator) {
+        if (size <= 1) {
+            return; // No need to sort
+        }
+
+        head = insertionSort(head, comparator);
+    }
+
+    // Insertion sort implementation for a linked list
+    private Node<T> insertionSort(Node<T> start, Comparator<T> comparator) {
+        Node<T> sorted = null; // Initialize an empty sorted list
+        Node<T> current = start; // Start with the original list
+
+        // Traverse the original list
+        while (current != null) {
+            Node<T> next = current.next; // Save the next node in the original list
+            sorted = insertSorted(sorted, current, comparator); // Insert current node into the sorted list
+            current = next; // Move to the next node in the original list
+        }
+
+        return sorted; // Return the sorted list
+    }
+
+    // Insert a node into the sorted position of the list using an external comparator
+    private Node<T> insertSorted(Node<T> sortedList, Node<T> newNode, Comparator<T> comparator) {
+        // If the sorted list is empty or the new node is smaller than the first node in the sorted list
+        if (sortedList == null || comparator.compare(newNode.data, sortedList.data) <= 0) {
+            newNode.next = sortedList; // Insert the new node at the beginning
+            return newNode; // Return the updated sorted list
+        }
+
+        Node<T> current = sortedList; // Start with the first node in the sorted list
+
+        // Find the correct position to insert the new node
+        while (current.next != null && comparator.compare(newNode.data, current.next.data) > 0) {
+            current = current.next; // Move to the next node in the sorted list
+        }
+
+        newNode.next = current.next; // Insert the new node into the sorted position
+        current.next = newNode; // Update the link in the sorted list
+
+        return sortedList;
+    }
+
+
+
 
     // Method to print the elements of the list
     public void printList() {
